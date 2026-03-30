5565a7ce33d8a07b781a4150a8c60c60ddf793b9
pengxianhong <290677893@qq.com>
2026-03-30 16:42:40 +0800

    refactor(MessageDispatcher): 调整消息内容格式化中的缩进

--- app/src/main/java/com/pengxh/daily/app/utils/MessageDispatcher.kt
+++ app/src/main/java/com/pengxh/daily/app/utils/MessageDispatcher.kt
@@ -25,11 +25,11 @@ class MessageDispatcher(private val context: Context, private val viewModel: Mes
             0 -> {
                 // 企业微信
                 val content = """
-                               标题：${title.ifBlank { messageTitle }}
-                               内容：${content.ifBlank { "未监听到打卡成功的通知，请手动登录检查" }}
-                               日期：$date
-                               版本号：${BuildConfig.VERSION_NAME}
-                               当前手机电量：${if (batteryCapacity >= 0) "$batteryCapacity%" else "未知"}
+                              标题：${title.ifBlank { messageTitle }}
+                              内容：${content.ifBlank { "未监听到打卡成功的通知，请手动登录检查" }}
+                              日期：$date
+                              版本号：${BuildConfig.VERSION_NAME}
+                              当前手机电量：${if (batteryCapacity >= 0) "$batteryCapacity%" else "未知"}
                               """.trimIndent()
                 viewModel.sendMessage(content, {}, {}, {})
             }
@@ -37,10 +37,10 @@ class MessageDispatcher(private val context: Context, private val viewModel: Mes
             1 -> {
                 // QQ邮箱
                 val content = """
-                               内容：${content.ifBlank { "未监听到打卡成功的通知，请手动登录检查" }}
-                               日期：$date
-                               版本号：${BuildConfig.VERSION_NAME}
-                               当前手机电量：${if (batteryCapacity >= 0) "$batteryCapacity%" else "未知"}
+                              内容：${content.ifBlank { "未监听到打卡成功的通知，请手动登录检查" }}
+                              日期：$date
+                              版本号：${BuildConfig.VERSION_NAME}
+                              当前手机电量：${if (batteryCapacity >= 0) "$batteryCapacity%" else "未知"}
                               """.trimIndent()
                 emailManager.sendEmail(title.ifBlank { messageTitle }, content, false)
             }
@@ -72,10 +72,10 @@ class MessageDispatcher(private val context: Context, private val viewModel: Mes
             1 -> {
                 // QQ邮箱
                 val content = """
-                               内容：${content}
-                               日期：$date
-                               版本号：${BuildConfig.VERSION_NAME}
-                               当前手机电量：${if (batteryCapacity >= 0) "$batteryCapacity%" else "未知"}
+                              内容：${content}
+                              日期：$date
+                              版本号：${BuildConfig.VERSION_NAME}
+                              当前手机电量：${if (batteryCapacity >= 0) "$batteryCapacity%" else "未知"}
                               """.trimIndent()
                 emailManager.sendAttachmentEmail(
                     title.ifBlank { messageTitle },
