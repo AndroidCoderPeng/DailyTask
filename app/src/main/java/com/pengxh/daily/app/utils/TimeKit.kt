6330730ee9bc2e22e84032566f6ded3155367d2b
Pengxh <290677893@qq.com>
2026-04-06 23:16:08 +0800

    refactor(utils): 移除 TimeKit 中的周日期范围功能
    - 删除了 getWeekDateRange 函数
    - 简化了 TimeKit 类的功能
    - 保留了基本的日期格式化功能

--- app/src/main/java/com/pengxh/daily/app/utils/TimeKit.kt
+++ app/src/main/java/com/pengxh/daily/app/utils/TimeKit.kt
@@ -10,18 +10,4 @@ object TimeKit {
     fun getTodayDate(): String {
         return dateFormat.format(Date())
     }
-
-    fun getWeekDateRange(): Pair<String, String> {
-        // 获取当前日期
-        val currentDate = Date()
-        val today = dateFormat.format(currentDate)
-
-        // 获取7天前的日期
-        val calendar = java.util.Calendar.getInstance()
-        calendar.time = currentDate
-        calendar.add(java.util.Calendar.DAY_OF_MONTH, -7)
-        val sevenDaysAgo = dateFormat.format(calendar.time)
-
-        return Pair(sevenDaysAgo, today)
-    }
 }
